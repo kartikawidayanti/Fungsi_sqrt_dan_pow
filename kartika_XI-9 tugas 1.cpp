6e1d550d69129311ee74aec7da5a8a5292c2035f
kartikawidayanti <141289529+kartikawidayanti@users.noreply.github.com>
2023-08-03 08:24:33 +0700

    Add files via upload

---
+++ kartika_XI-9 tugas 1.cpp
@@ -0,0 +1,19 @@
+de <iostream>
+#include <cmath>
+using namespa#include <cstdlib>
+#incluce std;
+int main()
+{
+	double x, Akar, Pangkat;
+	cout << "Masukkan Nilai x: ";
+	cin >> x;
+	
+	Akar = sqrt(x);
+	Pangkat = pow(x, 2);
+	
+cout << "Akar dari " << x << " = " << Akar << endl;
+cout <<"Pangkat dari "<< x << " = " <<Pangkat<< endl;
+
+	system ("PAUSE");
+	return 0;
+}
